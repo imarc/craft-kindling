403dfdf647889b09ef148bc3344cf8b64b863a3a
shawna@imarc.com <shawna@imarc.com>
2017-02-14 14:06:50 -0500

    fixed indentation within methods

--- twigextensions/WrapEmbedsExtension.php
+++ twigextensions/WrapEmbedsExtension.php
@@ -14,22 +14,22 @@ class WrapEmbedsExtension extends \Twig_Extension
 
     public function getName()
     {
-      return 'WrapEmbeds Twig Extension';
+        return 'WrapEmbeds Twig Extension';
     }
 
     public function getFilters()
     {
-      return array('wrapembeds' => new \Twig_Filter_Method($this, 'wrapembeds'));
+        return array('wrapembeds' => new \Twig_Filter_Method($this, 'wrapembeds'));
     }
 
     public function wrapembeds($content)
     {
-      $filtered = preg_replace('(<iframe.+?iframe>)si', '<div class="responsive_video">\0</div>', $content);
+        $filtered = preg_replace('(<iframe.+?iframe>)si', '<div class="responsive_video">\0</div>', $content);
 
-      $charset = craft()->templates->getTwig()->getCharset();
-      $filtered = new RichTextData($filtered, $charset);
+        $charset = craft()->templates->getTwig()->getCharset();
+        $filtered = new RichTextData($filtered, $charset);
 
-      return $filtered;
+        return $filtered;
     }
     
 }
