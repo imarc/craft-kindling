ebf94807629573ee340662b454d86e390e2c0111
Bill Bushee <bill@imarc.com>
2018-12-19 17:14:56 -0500

    Refactored to put twig extensions into subfolder. Added subfolder for variables.

---
+++ src/variables/KindlingVariable.php
@@ -0,0 +1,27 @@
+<?php
+use Craft;
+
+class KindlingVariable
+{
+    /**
+     * Provides public access to the craft()->userSession->setFlash method from your Twig template:
+     *
+     *     {{ craft.kindling.setFlash(name, value, default) }}
+     */
+    public function setFlash($name, $value, $default = null)
+    {
+        craft()->userSession->setFlash($name, $value, $default);
+
+        return true;
+    }
+
+    /**
+     * Debuging helper method returns a text string stating the microtime required to execute the script 
+     *
+     *     {{ craft.kindling.executionTime }}
+     */
+    public function executionTime()
+    {
+        return 'Page executed in ' . (microtime(true) - $_SERVER["REQUEST_TIME_FLOAT"]) . ' seconds';
+    }
+}
